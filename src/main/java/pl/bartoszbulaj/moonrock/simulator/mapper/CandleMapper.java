0ae38358baf472a5764d31b5019e1fb78de3cf82
bartosz.bulaj <external.bartosz.bulaj@kuehne-nagel.com>
2022-01-25 22:18:42 +0100

    Enhance CandleMapper

--- src/main/java/pl/bartoszbulaj/moonrock/simulator/mapper/CandleMapper.java
+++ src/main/java/pl/bartoszbulaj/moonrock/simulator/mapper/CandleMapper.java
@@ -1,6 +1,7 @@
 package pl.bartoszbulaj.moonrock.simulator.mapper;
 
 import org.jfree.data.xy.DefaultHighLowDataset;
+import pl.bartoszbulaj.moonrock.dto.InstrumentHistoryDto;
 import pl.bartoszbulaj.moonrock.simulator.model.Candle;
 import pl.bartoszbulaj.moonrock.simulator.model.CandleOHLC;
 
@@ -8,9 +9,13 @@ import java.util.List;
 
 public interface CandleMapper {
 
-	Candle map(CandleOHLC candleOHLC);
+	Candle mapToCandle(CandleOHLC candleOHLC);
 
 	CandleOHLC mapToOhlc(Candle candle);
+
+	Candle mapToCandle(InstrumentHistoryDto instrumentHistoryDto);
+
+	CandleOHLC mapToOhlc(InstrumentHistoryDto instrumentHistoryDto);
 
 	DefaultHighLowDataset mapToDefaultHighLowDataset(List<Candle> candleList);
 }
